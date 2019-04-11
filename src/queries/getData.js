45d7b7e6e612eb3ed1ff2ec91784e39f04e943af
Ania Blaziak <anblaziak@gmail.com>
2019-04-11 21:27:15 +0100

    initial css styling

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,20 +1,15 @@
-/// SELECT statemet here might not be right - it was not tested on the database
-
-const dbConnection = require('../database/db_connection.js');
+const dbConnection = require("../database/db_connection.js");
 
 const getData = cb => {
-  dbConnection.query(
-    'SELECT programme_name, description, length, continuity, users.name FROM programmes INNER JOIN users ON programmes.user_id = users.id;',
-    (err, res) => {
-      if (err) {
-        console.log('GET data error: ', err);
-        cb(err);
-      } else {
-        const suggestions = res.rows.slice();
-        cb(null, suggestions);
-      }
-    }
-  );
-};
+    dbConnection.query('SELECT programme_name, description, length, continuity, users.name FROM programmes INNER JOIN users ON user_id = users.id;', (err, res) => {
+        if (err) {
+            console.log("GET data error: ", err);
+            cb(err);
+        } else {
+            const suggestions = result.rows.slice();
+            cb(null, suggestions)
+        }
+    })
+}
 
 module.exports = getData;
