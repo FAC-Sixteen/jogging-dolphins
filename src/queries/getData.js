03b4510dc30bb0a90f30b47a47fe05ff9055b59b
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-04-11 18:01:50 +0100

    links dom to html

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,17 +1,20 @@
 /// SELECT statemet here might not be right - it was not tested on the database
 
-const dbConnection = require("../database/db_connection.js");
+const dbConnection = require('../database/db_connection.js');
 
 const getData = cb => {
-    dbConnection.query('SELECT programme_name, description, length, continuity, users.name FROM programmes INNER JOIN users ON programmes.user_id = users.id;', (err, res) => {
-        if (err) {
-            console.log("GET data error: ", error);
-            cb(err);
-        } else {
-            const suggestions = result.rows.slice();
-            cb(null, suggestions)
-        }
-    })
-}
+  dbConnection.query(
+    'SELECT programme_name, description, length, continuity, users.name FROM programmes INNER JOIN users ON programmes.user_id = users.id;',
+    (err, res) => {
+      if (err) {
+        console.log('GET data error: ', err);
+        cb(err);
+      } else {
+        const suggestions = result.rows.slice();
+        cb(null, suggestions);
+      }
+    }
+  );
+};
 
 module.exports = getData;
