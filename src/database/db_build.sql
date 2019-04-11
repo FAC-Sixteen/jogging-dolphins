794c722c2c33415a05457d2a342e352c3ee4e454
Henry <hrmstockdale@gmail.com>
2019-04-11 16:38:02 +0100

    getData fixed, new DevDeps, npm i

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,21 +1,22 @@
 BEGIN;
 
-DROP TABLE IF NOT EXISTS programmes, users CASCADE;
+DROP TABLE IF EXISTS programmes CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
-CREATE TABLE IF NOT EXISTS programmes (
+CREATE TABLE programmes (
     programme_id SERIAL PRIMARY KEY,
     programme_name VARCHAR(100) NOT NULL,
     description VARCHAR(200), 
     length INTEGER, 
     continuity VARCHAR(3), 
-    user_id VARCHAR(50)
+    user_id SERIAL 
 );
 
 INSERT INTO programmes (programme_name, description, length, continuity) VALUES ('Adventure Time', 'Cartoon, comedy', 11, 'N');
 INSERT INTO programmes (programme_name, description, length, continuity) VALUES ('Queer Eye', 'Fabulous reality show, makeovers', 43, 'N');
 INSERT INTO programmes (programme_name, description, length, continuity) VALUES ('Big Brother Canada', 'Reality show, features real Canadians', 40, 'Y');
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
@@ -24,4 +25,4 @@ INSERT INTO users (name) VALUES ('Rohan');
 INSERT INTO users (name) VALUES ('Henry');
 INSERT INTO users (name) VALUES ('Joko');
 
-COMMIT;
+COMMIT; 
