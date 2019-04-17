356e2620c28bf950701ea5ce35e635fbc310849e
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-04-17 18:17:47 +0100

    minor change to user table

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,28 +1,45 @@
 BEGIN;
 
-DROP TABLE IF EXISTS programmes CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+    DROP TABLE IF EXISTS programmes
+    CASCADE;
+DROP TABLE IF EXISTS users
+CASCADE;
 
-CREATE TABLE programmes (
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+INSERT INTO users
+    (name)
+VALUES
+    ('Rohan'),
+    ('Henry'
+),
+    ('Joko'
+);
+
+
+CREATE TABLE programmes
+(
     programme_id SERIAL PRIMARY KEY,
     programme_name VARCHAR(100) NOT NULL,
-    description VARCHAR(200), 
-    length INTEGER, 
-    continuity VARCHAR(3), 
-    user_id SERIAL 
+    description VARCHAR(200),
+    length INTEGER,
+    continuity VARCHAR(3),
+    user_id INTEGER REFERENCES users(id)
 );
 
-INSERT INTO programmes (programme_name, description, length, continuity) VALUES ('Adventure Time', 'Cartoon, comedy', 11, 'N');
-INSERT INTO programmes (programme_name, description, length, continuity) VALUES ('Queer Eye', 'Fabulous reality show, makeovers', 43, 'N');
-INSERT INTO programmes (programme_name, description, length, continuity) VALUES ('Big Brother Canada', 'Reality show, features real Canadians', 40, 'Y');
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+INSERT INTO programmes
+    (programme_name, description, length, continuity)
+VALUES
+    ('Adventure Time', 'Cartoon, comedy', 11, 'No'),
+    ('Queer Eye', 'Fabulous reality show, makeovers', 43, 'No'
+),
+    ('Big Brother Canada', 'Reality show, features real Canadians', 40, 'Yes'
 );
 
-INSERT INTO users (name) VALUES ('Rohan');
-INSERT INTO users (name) VALUES ('Henry');
-INSERT INTO users (name) VALUES ('Joko');
 
 COMMIT; 
